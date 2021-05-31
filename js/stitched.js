001ea4141ac3779ebda97e9047a5e3ccb21d7610
levi <levicaseblodgett@gmail.com>
2021-05-30 22:19:35 -0700

    cleaned + javascript + footer
    -cleaned up HTML elements and CSS classes
    -changed the background color of the navbar's dropdown menu's items
    -added a hover effect to the navbar's dropdown menu's items
    -added a youtube link to the navbar
    -changed the Spotify navbar item to Bandcamp
    -various other tweaks to HTML elements and their styling
    -created the "stitched.js" file and linked the HTML code to it
    -added several functions for opening links from the navbar
    -created the footer section at the bottom of the page
    -created an HTML file for the basic template so I can create the other pages of the site

---
+++ js/stitched.js
@@ -0,0 +1,29 @@
+/*opens the Instagram page in a new tab*/
+function openInstagram() {
+	window.open("https://www.instagram.com/stitchedrecordingsllc/");
+}
+
+/*opens the Facebook page in a new tab*/
+function openFacebook() {
+	window.open("https://www.facebook.com/StitchedRecordingsLLC");
+}
+
+/*opens the Twitter page in a new tab*/
+function openTwitter() {
+	window.open("https://twitter.com/StitchedRec");
+}
+
+/*opens the Youtube page in a new tab*/
+function openYoutube() {
+	window.open("https://www.youtube.com/user/StitchedRecordings");
+}
+
+/*opens the Soundcloud page in a new tab*/
+function openSoundcloud() {
+	window.open("https://soundcloud.com/stitched-recordings-llc");
+}
+
+/*opens the Bandcamp page in a new tab*/
+function openBandcamp() {
+	window.open("https://stitchedrecordingsllc.bandcamp.com/");
+}
